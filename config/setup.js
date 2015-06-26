942065e43b254cccd7c831e098425e7228f9faf2
Dave Pisek <dave.pisek@gmail.com>
2015-06-26 23:35:40 +1000

    Backend: Changes setup to use right public dir
    Points config/setup to the right public directory.

--- config/setup.js
+++ config/setup.js
@@ -9,17 +9,19 @@ var path = require('path');
 var express = require('express');
 var renderer = require('react-engine');
 
+var pathToPublic = path.join(__dirname, '../public');
+
 var engine = renderer.server.create({
-  reactRoutes: path.join(__dirname, '../public/routes.jsx')
+  reactRoutes: path.join(pathToPublic, 'routes.jsx')
 });
 
 module.exports = function (app) {
 
     // use jsx for rendering
     app.engine('.jsx', engine);
-    app.set('views', path.join(__dirname, '/public/views'));
+    app.set('views', path.join(pathToPublic, 'views'));
     app.set('view engine', 'jsx');
     app.set('view', renderer.expressView);
 
-    app.use(express.static(path.join(__dirname, '/public')));
+    app.use(express.static(pathToPublic));
 };
