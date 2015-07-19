d8ac83cc7e34e688aeffdb7358e6b34dbddd8fe8
Dave Pisek <dave.pisek@gmail.com>
2015-07-19 19:27:37 +1000

    Frontend: Adds rendering for daily view

--- public/views/forecasts/daily.jsx
+++ public/views/forecasts/daily.jsx
@@ -3,15 +3,35 @@
 'use strict';
 
 var React = require('react');
+var moment = require('moment');
 
 module.exports = React.createClass({
     displayName: 'forecastDaily',
+
     render: function render() {
+        var hourlyForecasts = this.props.forecast.data.hourly;
+        var day = moment.unix(this.props.forecast.data.currently.time).format('ddd, MMMM Do');
+
+        var hourlyForecastNodes = hourlyForecasts.data.map(function (forecast) {
+            var time = moment.unix(forecast.time).format('hA');
+            return (
+                <li key={forecast.time}>
+                    <h3>{time}</h3>
+                    <span className='summary'>{forecast.summary}</span>
+                </li>
+            );
+        });
+
         return (
             <div className='forecast'>
-                <h1>Daily forecast for: {this.props.location}</h1>
+                <h1>Forecast for: {day} - {this.props.location}</h1>
                 <h2>Summary</h2>
-                <p>{this.props.forecast.data.summary}</p>
+                <p>{hourlyForecasts.summary}</p>
+
+                <h2>Hourly Forecasts</h2>
+                <ul>
+                    {hourlyForecastNodes}
+                </ul>
             </div>
         );
     }
